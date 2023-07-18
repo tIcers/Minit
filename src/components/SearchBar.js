14e7482f7affcf6bc2a3e9e14b83e847b2601370
atsuki <csatsfapsfu2930@gmail.com>
2023-07-17 18:15:37 -0700

    impleented search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div style={searchContainerStyles}>
+      <h2>This is Search bar</h2>
+    </div>
+  )
+}
+
+const searchContainerStyles = {
+  margin:'auto',
+  marginRight:'100px'
+
+}
+export default SearchBar
