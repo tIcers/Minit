0f5a04d490a35771b4c5178a4addcdd29933f056
atsuki <csatsfapsfu2930@gmail.com>
2023-07-18 17:18:29 -0700

    implemented router and rendered

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import PostList from './components/PostList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PostDetails from './components/PostDetails';
 
 function App() {
   return (
-    <div className="App">
-       <NavBar/> 
-      <header className="App-header">
-        <div>
-          <PostList/>
-        </div>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<PostList/>}/>
+        <Route path='/post/:postId' element={<PostDetails/>}/>
+      </Routes>
+    </Router>
   );
 }
 
