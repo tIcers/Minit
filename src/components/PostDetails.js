59a2734fc2eb51bf79166f4f9da3df160726778c
atsuki <csatsfapsfu2930@gmail.com>
2023-07-18 17:18:50 -0700

    created post details with params

---
+++ src/components/PostDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const PostDetails = () => {
+  const {postId} = useParams()
+
+  return (
+    <div>
+      <h2>Post Details:</h2>
+      <p>Post ID: {postId}</p>
+    </div>
+  )
+}
+
+
+export default PostDetails
