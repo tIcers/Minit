0331a84e66f642dfb5856b1866e01a1f9808c511
atsuki <csatsfapsfu2930@gmail.com>
2023-07-19 22:16:13 -0700

    imported add commment and commentlist

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import PostList from "./PostList";
+import AddComment from "./AddComment";
+import CommentList from "./CommentList";
 
 const PostDetails = () => {
   const {postId} = useParams()
+  const [comments, setComments] = useState([])
 
+  const handleAddComment = (comment) => {
+    setComments([...comments, comment])
+
+  }
   return (
     <div>
       <h2>Post Details:</h2>
       <p>Post ID: {postId}</p>
+      <AddComment onAddComment={handleAddComment}/ >
+      <CommentList comments={comments}/>
+
     </div>
   )
 }
