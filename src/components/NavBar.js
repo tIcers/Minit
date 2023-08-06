c8ede9a6c72d770bb367d1b5f7dfa42659f01a60
atsuki <csatsfapsfu2930@gmail.com>
2023-08-06 11:59:37 -0700

    centered search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import SearchBar from './SearchBar'
 
 const NavBar = ({onSubredditChange}) => {
@@ -10,41 +10,51 @@ const NavBar = ({onSubredditChange}) => {
           <span style={letterStyle}>Minit</span>
         </div>
       </div>
-      <div >
-        <SearchBar onSubredditChange={onSubredditChange}/>
+      <div style={centerContainerStyles}> {/* Updated style for centerContainer */}
+        <SearchBar onSubredditChange={onSubredditChange} />
       </div>
+      <div style={rightContainerStyles}></div>
     </nav>
   );
 };
 
 const navStyles = {
-  display:'flex',
-  alignItems:'center',
-  justifyContent:'space-between',
-  height:'60px',
-  padding:'0 20px'
-}
-const leftContainerStyles = {
-  marginRight:'auto'
-}
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: '60px',
+  padding: '0 20px',
+  width: '100%', // Make sure the nav takes the full width of the screen
+};
 
+const leftContainerStyles = {};
+
+const centerContainerStyles = { // Added new style for centerContainer
+  flex: 1, // Make the centerContainer take available space
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+const rightContainerStyles = {
+  marginLeft: 'auto',
+};
 
 const imageStyle = {
-  height:'40px',
-  width:'auto'
-}
+  height: '40px',
+  width: 'auto',
+};
 
 const letterStyle = {
-  marginLeft:'8px',
-  fontSize:'18px',
-  display:'flex',
-  alignItems:'center',
-  color:'blue'
-}
+  marginLeft: '8px',
+  fontSize: '18px',
+  display: 'flex',
+  alignItems: 'center',
+  color: 'blue',
+};
+
 const imageContainerStyle = {
   display: 'flex',
   alignItems: 'center',
 };
-
 
 export default NavBar
