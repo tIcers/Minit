a0660299e6c84758d132877f66aeda4df3f0087e
atsuki <csatsfapsfu2930@gmail.com>
2023-08-14 01:25:02 -0700

    implemented modeswitch button

--- src/components/ModeSwitchButton.js
+++ src/components/ModeSwitchButton.js
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
 
-const ModeSwitchButton = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleMode = () => {
-    setIsDarkMode(prevMode => !prevMode);
-    if (isDarkMode) {
-      document.body.classList.remove("dark-mode");
-    } else {
-      document.body.classList.add("dark-mode");
-    }
-  };
-
+const ModeSwitchButton = ({ onToggle }) => {
   return (
-    <button onClick={toggleMode}>
-      {isDarkMode ? "Light Mode" : "Dark Mode"}
+    <button onClick={onToggle}>
+      Dark Mode
     </button>
   );
 };
